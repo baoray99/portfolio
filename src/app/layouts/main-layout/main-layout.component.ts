2c1b23f52b736b225e80f6b4fbd7b26dcc699035
ltbao <ltbao@vnpt.vn>
2024-07-19 17:17:15 +0700

    a

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,4 +1,6 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, HostListener, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
 declare var $: any
 declare var Typed: any
 @Component({
@@ -7,74 +9,89 @@ declare var Typed: any
   styleUrls: ['./main-layout.component.scss']
 })
 export class MainLayoutComponent implements OnInit, AfterViewInit {
+  navbar: any 
+  backtop: any
 
+
+  windowScroll$ = fromEvent(window, 'scroll').pipe(
+    map((e: Event) => {
+     
+    })
+  ).subscribe((next)=>{
+    console.log(next)
+  })
   constructor() { }
 
   ngOnInit(): void {
   }
 
   ngAfterViewInit(): void {
-    $(document).ready(function () {
-      //menu link click smooth
-      $("a")
-        .not('[href="#"]')
-        .not('[href="#0"]')
-        .click(function (e) {
-          var data_id = $(this).attr("href");
-          $("html, body").animate(
-            {
-              scrollTop: $(data_id).offset().top,
-            },
-            700
-          );
-        });
-      //typing animation
-      var type = new Typed(".typing", {
-        strings: ["Programmer.", "Coder.", "Designer.", "Gymmer."],
-        typeSpeed: 150,
-        backType: 120,
-        loop: true,
-      });
-      //navbar animation
-      $(window).scroll(function () {
-        if (this.scrollY > 20) {
-          $(".navbar").addClass("sticky");
-        } else {
-          $(".navbar").removeClass("sticky");
-        }
-        if (this.scrollY > 100) {
-          $(".backtop").addClass("show");
-        } else {
-          $(".backtop").removeClass("show");
-        }
-      });
-      //toggle menu/navbar
-      $(".menu-btn").click(function () {
-        $(".navbar .menu").toggleClass("active");
-        $(".menu-btn i").toggleClass("active");
-      });
-      // owl carousel use owl carousel library
-      $(".carousel").owlCarousel({
-        margin: 30,
-        loop: true,
-        autoplay:1000,
-        autoplayHoverPause: true,
-        responsive: {
-          0: {
-            items: 1,
-            nav: false,
-          },
-          600: {
-            items: 2,
-            nav: false,
-          },
-          1000: {
-            items: 3,
-            nav: false,
-          },
-        },
-      });
-    });
+    // $(document).ready(function () {
+    //   //menu link click smooth
+    //   $("a")
+    //     .not('[href="#"]')
+    //     .not('[href="#0"]')
+    //     .click(function (e) {
+    //       var data_id = $(this).attr("href");
+    //       $("html, body").animate(
+    //         {
+    //           scrollTop: $(data_id).offset().top,
+    //         },
+    //         700
+    //       );
+    //     });
+    //   //typing animation
+    //   var type = new Typed(".typing", {
+    //     strings: ["Programmer.", "Coder.", "Designer.", "Gymmer."],
+    //     typeSpeed: 150,
+    //     backType: 120,
+    //     loop: true,
+    //   });
+    //   //navbar animation
+    //   $(window).scroll(function () {
+    //     if (this.scrollY > 20) {
+    //       $(".navbar").addClass("sticky");
+    //     } else {
+    //       $(".navbar").removeClass("sticky");
+    //     }
+    //     if (this.scrollY > 100) {
+    //       $(".backtop").addClass("show");
+    //     } else {
+    //       $(".backtop").removeClass("show");
+    //     }
+    //   });
+    //   //toggle menu/navbar
+    //   $(".menu-btn").click(function () {
+    //     $(".navbar .menu").toggleClass("active");
+    //     $(".menu-btn i").toggleClass("active");
+    //   });
+    //   // owl carousel use owl carousel library
+    //   $(".carousel").owlCarousel({
+    //     margin: 30,
+    //     loop: true,
+    //     autoplay:1000,
+    //     autoplayHoverPause: true,
+    //     responsive: {
+    //       0: {
+    //         items: 1,
+    //         nav: false,
+    //       },
+    //       600: {
+    //         items: 2,
+    //         nav: false,
+    //       },
+    //       1000: {
+    //         items: 3,
+    //         nav: false,
+    //       },
+    //     },
+    //   });
+    // });
+
+    this.navbar = document.querySelector('.navbar')
+    this.backtop = document.querySelector('.backtop')
+
+    console.log(this.backtop, this.navbar);
     
   }
 
